5b54895a05abab64f3b9b3fd8c24cc88db2e69de
fadhiladityaa <fadhiladitya551@gmail.com>
2026-07-08 20:58:30 +0800

    menambahkan navigasi sederhana ke 3 file

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About</title>
+    <title>Contact</title>
 </head>
 <body>
-    <h1>Ini adalah halaman About</h1>
+    <h1>Ini adalah halaman Contact</h1>
      <ul>
         <li><a href="/home">Home</a></li>
         <li><a href="/about">About</a></li>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h1>Ini adalah halaman home</h1>
+    <ul>
+        <li><a href="/home">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+    </ul>
 </body>
 </html>
