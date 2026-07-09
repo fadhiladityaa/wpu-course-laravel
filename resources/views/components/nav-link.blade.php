2443427e489226630dcc282eb2709f3c294f1575
fadhiladityaa <fadhiladitya551@gmail.com>
2026-07-09 20:58:44 +0800

    belajar attributes dan props

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,4 +1,15 @@
 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
+@props(['href', 'current' => false, 'ariaCurrent' => false])
 
-<a href="{{ $href }}" aria-current="page"
-    class="{{ $current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium">{{ $slot }}</a>
+@php
+    if ($current) {
+        $classes = 'bg-gray-900 text-white';
+        $ariaCurrent = 'page';
+    } else {
+        $classes = 'text-gray-300 hover:bg-white/5 hover:text-white';
+    }
+@endphp
+
+<a href="{{ $href }}"
+    {{ $attributes->merge(['class' => 'rounded-md px-3 py-2 text-sm font-medium ' . $classes, 'aria-current' => $ariaCurrent]) }}>
+    {{ $slot }}</a>
