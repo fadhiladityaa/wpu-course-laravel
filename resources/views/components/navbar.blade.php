ea22cb10ceae7c9cedbfa4b1b43c564df68d4b5e
fadhiladityaa <fadhiladitya551@gmail.com>
2026-07-09 22:32:42 +0800

    perbaiki navlink untuk mobile

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -65,16 +65,12 @@
             </div>
 
             <el-disclosure id="mobile-menu" hidden class="block md:hidden">
-                <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+                <div class="space-y-1 flex flex-col px-2 pt-2 pb-3 sm:px-3">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                    <a href="#" aria-current="page"
-                        class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Home page</a>
-                    <a href="#"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Blog</a>
-                    <a href="#"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                    <a href="#"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                    <x-nav-link href="/" :current="request()->is('/')">Home</x-nav-link>
+                    <x-nav-link href="/blog" :current="request()->is('blog')">Blog</x-nav-link>
+                    <x-nav-link href="/about" :current="request()->is('about')">About</x-nav-link>
+                    <x-nav-link href="/contact" :current="request()->is('contact')">Contact</x-nav-link>
 
                 </div>
                 <div class="border-t border-white/10 pt-4 pb-3">
