9442360d001ebd3b8e7b372c919e9c036b04116d
fadhiladityaa <fadhiladitya551@gmail.com>
2026-07-09 09:58:36 +0800

    sekarang halaman about sudah pakai blade tamplate inheritance

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full bg-gray-100">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <title>@yield('title')</title>
+</head>
+
+<body x-data="{ test: 'Blog', nama: 'Fadhil Aditya' }" class="h-full">
+    <div class="min-h-full">
+        {{-- navbar --}}
+        @include('partials.navbar')
+        <header class="relative bg-white shadow-sm">
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $title }}</h1>
+            </div>
+        </header>
+        <main>
+            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                <!-- Your content -->
+                @yield('container')
+            </div>
+        </main>
+    </div>
+
+</body>
+
+</html>
