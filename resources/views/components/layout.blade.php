7788f796553d14d3b348131f6c6602273228e753
fadhiladityaa <fadhiladitya551@gmail.com>
2026-07-09 10:27:34 +0800

    mengubah semuanya jadi sistem component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,22 +9,26 @@
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <title>@yield('title')</title>
+    <title>{{ $title }}</title>
 </head>
 
 <body x-data="{ test: 'Blog', nama: 'Fadhil Aditya' }" class="h-full">
     <div class="min-h-full">
-        {{-- navbar --}}
-        @include('partials.navbar')
-        <header class="relative bg-white shadow-sm">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $title }}</h1>
-            </div>
-        </header>
+        <x-navbar />
+
+        <x-header />
+
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <!-- Your content -->
-                @yield('container')
+                <div class="flex">
+                    @for ($i = 1; $i <= 10; $i++)
+                        @if ($i % 2 !== 0)
+                            <div class="p-3 bg-teal-300 me-1 text-slate-800 text-center">{{ $i }}</div>
+                        @endif
+                    @endfor
+                </div>
+                {{ $slot }}
             </div>
         </main>
     </div>
