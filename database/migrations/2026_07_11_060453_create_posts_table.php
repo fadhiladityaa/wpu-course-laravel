a71e378a71c315d7bd02929632f3672e4596102f
fadhiladityaa <fadhiladitya551@gmail.com>
2026-07-11 14:07:34 +0800

    edit file posts migrations

--- database/migrations/2026_07_11_060453_create_posts_table.php
+++ database/migrations/2026_07_11_060453_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('author');
+            $table->text('body');
             $table->timestamps();
         });
     }
