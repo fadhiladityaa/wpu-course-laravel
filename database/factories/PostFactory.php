e51af2c0e8e437c1166641a782b484720d18584e
fadhiladityaa <fadhiladitya551@gmail.com>
2026-07-17 19:14:49 +0800

    menambahkan faker pada post factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends Factory<Post>
@@ -17,8 +18,12 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->sentence(rand(6, 8));
         return [
-            //
+            'title' => $title,
+            'slug' => Str::of($title)->slug('-'),
+            'author' => fake()->name(),
+            'body' => fake()->text(300),
         ];
     }
 }
